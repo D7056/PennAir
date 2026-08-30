52b62966681e183a81a59e9a7bfdf7de71b79e06
dararcho <dararcho@sas.upenn.edu>
2026-08-30 14:10:24 -0400

    Add contour detection and visualization to image processing pipeline

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import cv2
 from scipy.ndimage import median_filter
 import numpy as np
+from skimage.filters import apply_hysteresis_threshold
 
 img=cv2.imread("imgs/static.png")
 
@@ -12,16 +13,87 @@ kernel_size=(window_size,window_size, 1)
 
 
 
-median_img=median_filter(blurred_img, kernel_size)
 
-absolute=np.abs(median_img-blurred_img)
+
+absolute=cv2.absdiff(img,blurred_img)
+
 
 mad=median_filter(absolute,kernel_size)
+
+print(mad)
+print(np.max(mad))
+print(np.mean(mad))
+
+mad_display = cv2.cvtColor(mad, cv2.COLOR_BGR2GRAY)
+
+mad_display= cv2.normalize(mad_display, None, 0, 255, cv2.NORM_MINMAX)
+
+
+edges= absolute/(mad+0.3)
+
+edges=edges.astype(np.float32)
+
+
+
+
+edges_display = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
+
+connected=apply_hysteresis_threshold(edges_display, 2, 6)
+
+print("min:", edges_display.min())
+print("max:", edges_display.max())
+print("mean:", edges_display.mean())
+print("median:", np.median(edges_display))
+print("90th percentile:", np.percentile(edges_display, 90))
+print("99th percentile:", np.percentile(edges_display, 99))
+print("99.9th percentile:", np.percentile(edges_display, 99.9))
+
+edges_display= cv2.normalize(edges_display, None, 0, 255, cv2.NORM_MINMAX)
+
+
+
+connected_display = (connected.astype(np.uint8) * 255)
+
+kernel = np.ones((3, 3), np.uint8)
+closed = cv2.dilate(connected_display, kernel, iterations=1)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13, 13))
+closed = cv2.morphologyEx(closed, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+
+
+
+
+contour, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print(contour)
+min_area = 100
+
+filtered_contours = [c for c in contour if cv2.contourArea(c) >= min_area]
+
+
+
+print(len(filtered_contours))
+
+for i in filtered_contours:
+    print(cv2.contourArea(i))
+
+smoothed_contours = []
+for c in filtered_contours:
+    epsilon = 0.01 * cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, epsilon, True)
+    smoothed_contours.append(approx)
 
 
 
 cv2.imshow("Original Image", img)
 cv2.imshow("Blurred", blurred_img)
-cv2.imshow("MAD", mad)
+cv2.imshow("MAD", mad_display)
+cv2.imshow("Absolute", absolute)
+cv2.imshow("edges", edges_display)
+cv2.imshow("connected", connected_display)
+cv2.imshow("closed", closed)
+cv2.drawContours(img, smoothed_contours, -1, (0,0,0), 2)
+cv2.imshow("Drawn Contour", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
